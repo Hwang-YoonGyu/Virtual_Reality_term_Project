7a87a75dd191ea0e81152eede8cf477c977c382e
yoongyu-hwang <dotheji11@naver.com>
2021-12-04 13:29:14 +0900

    2021.12.04 13:30

--- Assets/Script/OnPointer.cs
+++ Assets/Script/OnPointer.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Collider))]
 public class OnPointer : MonoBehaviour
 {
-    public enum Role { Enemy, Screen, ExitScreen}
+    public enum Role { Enemy, Screen, ExitScreen, Loader}
     public Role role;
     public Image LoadingBar;
     private bool IsOn;
@@ -23,17 +23,15 @@ public class OnPointer : MonoBehaviour
     //------------------------------------------------------
     // Screen, ExitScreen 전용 전용
 
-    public GameObject PlayerGunner;
-    public GameObject PlayerInner;
+    public GameObject Player;
+    public GameObject PlayerTurret;
     public GameObject GunnerCanvas;
-    public GameObject InnerCanvas;
+    
     //-----------------------------------------------------
     bool enemyIsAlive = true;
     void Start()
     {
         st = GameObject.Find("SceneSetting").GetComponent<SceneSetting>();
-
-
         LoadingBar = GameObject.Find("LoadingBar").GetComponent<Image>();
         IsOn = false;
         LoadingBar.fillAmount = 0;
@@ -89,6 +87,9 @@ public class OnPointer : MonoBehaviour
             case Role.ExitScreen:
                 ExitScreen();
                 break;
+            case Role.Loader:
+                OnLoad();
+                break;
         }
 
     }
@@ -101,32 +102,34 @@ public class OnPointer : MonoBehaviour
             Instantiate(BombPrefab, EnemyTank.GetComponent<Transform>().position, EnemyTank.GetComponent<Transform>().rotation);
             EnemyTankTurret.GetComponent<Rigidbody>().AddForce(new Vector3(20, 300, 20));
             barTime = 0.0f; // 바로 상호작용하는것을 막기위해 barTime 초기화
-            enemyIsAlive = false; 
-            
+            enemyIsAlive = false;
+            st.killCount++; // SceneSetting의 killCount를 1 추가
+
         }
         else if (!enemyIsAlive) 
         {
             Destroy(gameObject);
             barTime = 0.0f;
             st.isEnemy = false; //적전차가 파괴되었고 게임상에서도 삭제되었음을 SceneSetting에게 알림
-            st.killCount++; // SceneSetting의 killCount를 1 추가
+            
         }
     }
 
     void GotoAim() {
         Debug.Log("Goto Aim");
-        PlayerGunner.SetActive(true);
+        Player.transform.position = new Vector3(72.67604f, -2.164f, 13.46046f);
+        PlayerTurret.SetActive(true);
         GunnerCanvas.SetActive(true);
-        PlayerInner.SetActive(false);
-        InnerCanvas.SetActive(false);
     }
     void ExitScreen()
     {
         Debug.Log("Exit Screen");
-        PlayerInner.SetActive(true);
-        InnerCanvas.SetActive(true);
-        PlayerGunner.SetActive(false);
+        Player.transform.position = new Vector3(0.928f, 3.499f, 0.018f);
+        PlayerTurret.SetActive(false);
         GunnerCanvas.SetActive(false);
-
+    }
+    void OnLoad()
+    {
+        
     }
 }
--- Assets/Script/SceneSetting.cs
+++ Assets/Script/SceneSetting.cs
@@ -1,29 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneSetting : MonoBehaviour
 {
     public int killCount =0;
     public bool isEnemy = true;
-    //---------------------------------------
-    
-    public GameObject Enemy;
     public bool isLoad = false;
+    public Text t;
+
+    //---------------------------------------
+
+    public GameObject Enemy;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(Enemy, new Vector3(63, -2, 12), new Quaternion(0, 0, 0, 0));
+        //t.text = "적 전차 파괴수 : " + killCount + "\n" + "장전여부 : " + isLoad;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        t.text = "적 전차 파괴수 : " + killCount + "\n\n" + "장전여부 : " + isLoad;
         if (!isEnemy) {
-            
-            
+
+           
             Instantiate(Enemy, new Vector3(63, -2, 12), new Quaternion(0, 0, 0, 0));
             isEnemy = true;
 
